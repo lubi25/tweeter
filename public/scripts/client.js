925775117baebc31c1c97f9138df2875a8dc6fd8
Lubi Islam <lubaba.islam91@gmail.com>
2024-03-31 20:19:32 -0400

    Edit createTweetElement function to take in 'tweetData' parameter to access correct properties inside const data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,32 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-function createTweetElement() {
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+function createTweetElement(tweetData) {
   let $tweet = `
     <section class="tweet">
     <header>
@@ -41,20 +66,7 @@ function renderTweets(tweets) {
   });
 }
 
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
 $(document).ready(function() {
-  const $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
+  console.log(data);
 });
