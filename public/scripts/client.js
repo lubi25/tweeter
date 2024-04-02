32e7eed8c8b35eedde7cd9f694a0be813ef5095c
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-02 00:26:13 -0400

    Add loadTweets function to document ready

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 function createTweetElement(tweetData) {
   let $tweet = `
@@ -81,13 +57,25 @@ function postTweet(formData) {
 }
 
 $(document).ready(function() {
-  renderTweets(data);
-
   $('.new-tweet form').on('submit', function(e) {
     e.preventDefault();
-
     var formData = $(this).serialize();
-
     postTweet(formData);
   });
+
+  function loadTweets() {
+    $.ajax('/tweets', {
+      method: "GET",
+      dataType: "json",
+      success: function(receivedTweets) {
+        renderTweets(receivedTweets);
+        console.log('Tweets retrieved!');
+      },
+      error: function(xhr, status, error) {
+        console.log('An error occurred when retrieving tweets. Please try again later.');
+      }
+    });
+  }
+
+  loadTweets();
 });
