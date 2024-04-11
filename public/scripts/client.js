f4bbb8f93a017c8555148102fa77d4d13694006a
Lubi Islam <lubaba.islam91@gmail.com>
2024-04-10 21:16:13 -0400

    Move loadTweets function to fire only upon successful /POST

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,13 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+/* Escape special characters for cross-site scripting */
 
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
+
+
+/* Create a new Tweet element with HTML */
 
 function createTweetElement(tweetData) {
   const timeAgo = timeago.format(tweetData.created_at); 
@@ -43,6 +42,9 @@ function createTweetElement(tweetData) {
   return $tweet;
 }
 
+
+/* Render new tweets to appear at the top of the tweets container */
+
 function renderTweets(tweets) {
   tweets.forEach(tweetEntry => {
     const $newTweetEntry = createTweetElement(tweetEntry);
@@ -50,11 +52,15 @@ function renderTweets(tweets) {
   });
 }
 
+
+/* Post a new tweet; loadTweets() reloads the tweets once successfully posted */
+
 function postTweet(formData) {
   $.ajax('/tweets', {
     method: "POST",
     data: formData,
     success: function(response) {
+      loadTweets();
       $('#tweet-text').val('');
       console.log('Tweet posted successfully!');
     },
@@ -63,6 +69,9 @@ function postTweet(formData) {
     }
   });
 }
+
+
+/* Load tweets from /tweets */
 
 function loadTweets() {
   $.ajax('/tweets', {
@@ -80,12 +89,20 @@ function loadTweets() {
   });
 }
 
+
+/* Reset character counter to '140' once postTweets is successful */
+
 function counterReset() {
   let counter = $('.counter');
   counter.text('140');
 }
 
+
+/* Initial tweets/page load and event handling for errors while posting tweets */
+
 $(document).ready(function() {
+  loadTweets();
+
   $('.new-tweet form').on('submit', function(e) {
     e.preventDefault();
     let tweetTextArea = $(this).find('textarea[name="text"]');
@@ -94,17 +111,14 @@ $(document).ready(function() {
   
     if (tweetText.length === 0) {
       $(".error").slideDown(500).addClass("active");
-      error.text('No tweet submitted!');
+      error.text('Cannot post an empty tweet!');
     } else if (tweetText.length > 140) {
       $(".error").slideDown(500).addClass("active");
       error.text('Tweet is too long!');
     } else {
       $(".error").slideUp(500).removeClass("active");
       postTweet($(this).serialize());
-      loadTweets();
       counterReset();
     }
   });
-
-  loadTweets();
 });
